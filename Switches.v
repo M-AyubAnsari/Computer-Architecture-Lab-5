06c6ab46586659f9cbef1d7d80cb43605f96580d
M-AyubAnsari <ma10034@st.habib.edu.pk>
2026-02-16 00:19:00 +0500

    Switches.v Uploaded

---
+++ Switches.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+module switches(
+    input clk,
+    input rst,
+    input [31:0] writeData,
+    input writeEnable,
+    input readEnable,
+    input [29:0] memAddress,
+    output reg [31:0] readData = 0, // not to be read
+    output reg [15:0] leds
+    );
+endmodule
