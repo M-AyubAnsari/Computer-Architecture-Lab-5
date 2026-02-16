5f19153ef8a52f7a4ba98e35d6c20d5db1d9db29
M-AyubAnsari <ma10034@st.habib.edu.pk>
2026-02-16 21:16:00 +0500

    Updated Switches.v Uploaded

--- Switches.v
+++ Switches.v
@@ -1,12 +1,25 @@
 `timescale 1ns / 1ps
+
 module switches(
     input clk,
     input rst,
+    input [15:0] btns,
     input [31:0] writeData,
     input writeEnable,
     input readEnable,
     input [29:0] memAddress,
-    output reg [31:0] readData = 0, // not to be read
-    output reg [15:0] leds
-    );
+    input [15:0] switches,
+
+    output reg [31:0] readData
+);
+
+parameter SWITCH_ADDR = 30'h00000000;
+
+always @(*) begin
+    if (readEnable && memAddress == SWITCH_ADDR)
+        readData = {16'd0, switches};
+    else
+        readData = 32'd0;
+end
+
 endmodule
