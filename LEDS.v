0786287cd9ac95c9723ae06d178f00592747f179
M-AyubAnsari <ma10034@st.habib.edu.pk>
2026-02-16 21:15:14 +0500

    Updated LEDS.v Uploaded

--- LEDS.v
+++ LEDS.v
@@ -7,15 +7,29 @@ module leds(
     input writeEnable,
     input readEnable,
     input [29:0] memAddress,
-    
-    output reg  [31:0] readData = 0, // not to be read       
+
+    output reg [31:0] readData = 0,
     output reg [15:0] leds
-    );
-    
-    reg [7:0] ledData [3:0];
-    
-   
+);
+
+parameter LED_ADDR = 30'h00000004;
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        leds <= 16'd0;
+    end
+    else begin
+        if (writeEnable && memAddress == LED_ADDR) begin
+            leds <= writeData[15:0];
+        end
+    end
+end
+
+always @(*) begin
+    if (readEnable && memAddress == LED_ADDR)
+        readData = {16'd0, leds};
+    else
+        readData = 32'd0;
+end
+
 endmodule
-
-
-
